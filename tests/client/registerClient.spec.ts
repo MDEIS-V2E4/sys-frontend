a51a003da31b8c1aace7fb2472d15eae99174d7f
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2025-03-19 16:34:16 -0400

    49-e2e-refactor-registerclient-test

--- tests/client/registerClient.spec.ts
+++ tests/client/registerClient.spec.ts
@@ -1,17 +1,24 @@
 import { test, expect } from '@playwright/test';
 
-test('Register new client', async ({ page }) => {
-  await page.goto(
-    'https://sys-frontend-develop-3y58e.ondigitalocean.app/cliente',
-  );
-  await page.getByRole('textbox', { name: 'Nombre:' }).click();
-  await page.getByRole('textbox', { name: 'Nombre:' }).fill('aldo balderrama');
+test('Register a new client', async ({ page }) => {
+  await page.goto('https://sys-frontend-develop-3y58e.ondigitalocean.app/');
+  await page.getByRole('link', { name: 'Clientes' }).click();
+  await page.getByRole('heading', { name: 'Clientes' }).getByRole('button').click();
+  await page.getByRole('textbox', { name: 'Nombre:' }).fill('aldo UI 34');
   await page.getByRole('textbox', { name: 'Nro. CI/NIT:' }).click();
-  await page.getByRole('textbox', { name: 'Nro. CI/NIT:' }).fill('12345');
-  await page.getByRole('textbox', { name: 'Email:' }).click();
-  await page.getByRole('textbox', { name: 'Email:' }).fill('aldo@autobook.com');
+  await page.getByRole('textbox', { name: 'Nro. CI/NIT:' }).fill('123456789');
+  await page.getByRole('textbox', { name: 'Correo:' }).click();
+  await page.getByRole('textbox', { name: 'Correo:' }).fill('aldotest@autobots.com');
   await page.getByRole('button', { name: 'Registrar Cliente' }).click();
-  await expect(
-    page.locator('//div[contains(text(), "Guardado correctamente")]'),
-  ).toContainText('Guardado correctamente');
+  await page.waitForTimeout(1000);
+  await page.getByRole('link', { name: 'Empleados' }).click();
+  await page.waitForLoadState();
+  await page.waitForTimeout(1000);
+  await page.getByRole('link', { name: 'Clientes' }).click();
+  await page.waitForLoadState();
+  await expect(page.getByRole('cell', { name: 'aldo UI 34' })).toBeVisible();
+
+  // After this test, the client is deleted
+  await page.getByRole('row', { name: 'aldo UI 34 123456789 CI' }).getByRole('button').nth(1).click();
+  await expect(page.getByRole('cell', { name: 'aldo UI 34' })).toBeHidden();
 });
